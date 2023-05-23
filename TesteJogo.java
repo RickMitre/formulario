3f01332398a113cf32126b893256512a23e58afd
RickMitre <104009821+RickMitre@users.noreply.github.com>
2023-05-23 11:59:04 -0300

    Add files via upload

---
+++ TesteJogo.java
@@ -0,0 +1,12 @@
+package fejiow;
+
+public class TesteJogo {
+	public static void main(String[] args) {
+		Jogo jogo = new Jogo();
+		
+		jogo.core();
+		// 00 01 02
+		// 10 11 12
+		// 20 21 22
+	}
+}
